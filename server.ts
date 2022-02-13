3b07852ef7cf875a5c2251a6a1e53f5ed845a907
Geri <luga.geri@gmail.com>
2022-02-13 21:33:11 +0100

    final code

--- server.ts
+++ server.ts
@@ -1,8 +1,15 @@
 import express from "express";
 import cors from "cors";
 
+
 const app = express();
 const PORT = 3009;
+let i =0
+
+// app.set('json spaces', 10);
+// app.set('json replacer', (key:string,value:string)=>{
+//   return value.toUpperCase()
+// });
 
 const quotes = [
   {
@@ -14,6 +21,7 @@ const quotes = [
   { id: 3, text: "I have a question", author: "Geri Luga" },
   { id: 4, text: "-", author: "Everyone when Nico asks a question" },
 ];
+
 
 app.listen(PORT, () => {
   return console.log(`Server.ts started on port ${PORT}`);
@@ -29,9 +37,11 @@ app.use(
   })
 );
 app.get("/quotes", (req, res) => {
-  console.log(req.method +'  '+ res.statusCode + '  ' + req.originalUrl)
+  // res.json('adasdsda')
   if (Object.keys(req.query).length !== 0) {
     if (req.query.authorQ !== undefined && req.query.textQ !== undefined) {
+      console.log(req.method +' ('+i+') '+ res.statusCode + '  ' + req.url)
+  i++
       res.send(
         quotes.filter(
           (quote) =>
@@ -40,30 +50,39 @@ app.get("/quotes", (req, res) => {
         )
       );
     } else if (req.query.authorQ !== undefined) {
+      console.log(req.method +' ('+i+') '+ res.statusCode + '  ' + req.url)
+  i++
       res.send(
         quotes.filter((quote) =>
           quote.author.toLowerCase().includes(req.query.authorQ as string)
         )
       );
     } else if (req.query.textQ !== undefined) {
+      console.log(req.method +' ('+i+') '+ res.statusCode + '  ' + req.url)
+  i++
       res.send(
         quotes.filter((quote) => quote.text.toLowerCase().includes(req.query.textQ as string))
       );
     }
   } else {
+    console.log(req.method +' ('+i+') '+ res.statusCode + '  ' + req.path)
+  i++
     res.send(quotes);
   }
 });
 
 app.get("/random", (req, res) => {
-  console.log(req.method +'  '+ res.statusCode + '  ' + req.path)
+  console.log(req.method +' ('+i+') '+ res.statusCode + '  ' + req.path)
+  i++
   res.send(quotes[Math.floor(Math.random() * quotes.length)]);
 });
 
 app.get("*", (req, res) => {
   res.status(404).send({ error: "not found" });
-  console.log(req.method +'  '+ res.statusCode + '  ' + req.path)
-  
+  console.log(req.method +' ('+i+') '+ res.statusCode + '  ' + req.path)
+  i++
 });
+
+
 
 // fs.writeFile()
