cb7106f968ba5886004901169482118a12e7e85e
Geri <luga.geri@gmail.com>
2022-02-14 15:29:39 +0100

    Changed data format

--- server.ts
+++ server.ts
@@ -3,16 +3,64 @@ import cors from "cors";
 
 const app = express();
 const PORT = 3009;
+let i = 0;
+
+// app.set('json spaces', 10);
+// app.set('json replacer', (key:string,value:string)=>{
+//   return value.toUpperCase()
+// });
 
 const quotes = [
   {
     id: 1,
     text: "Let me shaare myyyyy screeeeeeeeen",
-    author: "Nicolas Marcora",
+    author: {
+      firstName: "Nicolas",
+      lastName: "Marcora",
+      photo: "https://robohash.org/NicolasMarcora",
+      age: 34,
+    },
   },
-  { id: 2, text: "I will bite", author: "Ed Putans" },
-  { id: 3, text: "I have a question", author: "Geri Luga" },
-  { id: 4, text: "-", author: "Everyone when Nico asks a question" },
+  {
+    id: 2,
+    text: "I will bite",
+    author: {
+      firstName: "Ed",
+      lastName: "Putans",
+      photo: "https://robohash.org/EdPutans",
+      age: 28,
+    },
+  },
+  {
+    id: 3,
+    text: "I have a question",
+    author: {
+      firstName: "Geri",
+      lastName: "Luga",
+      age: 24,
+      photo: "https://robohash.org/GeriLuga",
+    },
+  },
+  {
+    id: 4,
+    text: "-",
+    author: {
+      firstName: "Everyone when Nico",
+      lastName: "asks a question",
+      age: 0,
+      photo: "https://robohash.org/Hoxton",
+    },
+  },
+  {
+    id: 5,
+    text: "If you do a HEAD request, will the server give you head 🤔",
+    author: {
+      firstName: "Not",
+      lastName: "Geri",
+      age: 24,
+      photo: "https://robohash.org/NotGeri",
+    },
+  },
 ];
 
 app.listen(PORT, () => {
@@ -29,41 +77,64 @@ app.use(
   })
 );
 app.get("/quotes", (req, res) => {
-  console.log(req.method +'  '+ res.statusCode + '  ' + req.originalUrl)
+  // res.json('adasdsda')
   if (Object.keys(req.query).length !== 0) {
     if (req.query.authorQ !== undefined && req.query.textQ !== undefined) {
+      console.log(
+        req.method + " (" + i + ") " + res.statusCode + "  " + req.url
+      );
+      i++;
       res.send(
         quotes.filter(
           (quote) =>
-            quote.author.toLowerCase().includes(req.query.authorQ as string) &&
+            (
+              quote.author.firstName.toLowerCase() + quote.author.lastName.toLowerCase()
+            ).includes(req.query.authorQ as string) &&
             quote.text.toLowerCase().includes(req.query.textQ as string)
         )
       );
     } else if (req.query.authorQ !== undefined) {
+      console.log(
+        req.method + " (" + i + ") " + res.statusCode + "  " + req.url
+      );
+      i++;
       res.send(
         quotes.filter((quote) =>
-          quote.author.toLowerCase().includes(req.query.authorQ as string)
+          (
+            quote.author.firstName.toLowerCase() + quote.author.lastName.toLowerCase()
+          ).includes(req.query.authorQ as string)
         )
       );
     } else if (req.query.textQ !== undefined) {
+      console.log(
+        req.method + " (" + i + ") " + res.statusCode + "  " + req.url
+      );
+      i++;
       res.send(
-        quotes.filter((quote) => quote.text.toLowerCase().includes(req.query.textQ as string))
+        quotes.filter((quote) =>
+          quote.text.toLowerCase().includes(req.query.textQ as string)
+        )
       );
     }
   } else {
+    console.log(
+      req.method + " (" + i + ") " + res.statusCode + "  " + req.path
+    );
+    i++;
     res.send(quotes);
   }
 });
 
 app.get("/random", (req, res) => {
-  console.log(req.method +'  '+ res.statusCode + '  ' + req.path)
+  console.log(req.method + " (" + i + ") " + res.statusCode + "  " + req.path);
+  i++;
   res.send(quotes[Math.floor(Math.random() * quotes.length)]);
 });
 
 app.get("*", (req, res) => {
   res.status(404).send({ error: "not found" });
-  console.log(req.method +'  '+ res.statusCode + '  ' + req.path)
-  
+  console.log(req.method + " (" + i + ") " + res.statusCode + "  " + req.path);
+  i++;
 });
 
 // fs.writeFile()
